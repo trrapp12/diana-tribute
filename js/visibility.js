88922f936ca9c5059ef0d8e1642500a96bd5f03f
Trevor Rapp <trrapp@gmail.com>
2021-12-18 03:45:31 -0700

    adds a super snazzy, first-time-I-ever-successfully-used-it, actually shouted for joy IntersectionObserver with a callback function

--- js/visibility.js
+++ js/visibility.js
@@ -1,3 +1,32 @@
+// (() => {
+//   console.log('visibility.js is fired');
+//     let observer = new IntersectionObserver( function (entries, observer) {
+//       entries.forEach(entry => {
+//         if(entry.isIntersecting) {
+//           console.log("entry is intersecting" + entry.target.innerHTML);
+//           entry.target.classList.add('javascript-card-visibility');
+//           observer.unobserve(entry.target);
+//         }
+//       });
+//     }, {threshold: 1});
+//
+//   document.querySelectorAll('card').forEach(element => {observer.observe(element); });
+//
+// })();
+
 (() => {
+
+let observer = new IntersectionObserver(myObserverCallback, {threshold: 0.3});
+
+function myObserverCallback(entries, observer) {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('javascript-card-visibility');
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+document.querySelectorAll('.card:nth-child(even)').forEach(element => {observer.observe(element); });
 
 })();
