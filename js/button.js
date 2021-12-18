690af21b652a849bd4c48878e5f29ae7be3f9ba6
Trevor Rapp <trrapp@gmail.com>
2021-12-18 01:30:10 -0700

    enhances buttons functionality

--- js/button.js
+++ js/button.js
@@ -3,12 +3,22 @@
   const button = document.getElementById('button');
 
   button.addEventListener('click', (evt) => {
-    if (evt.target.className === 'inner-arrow' && count <= 29) {
+    if (evt.target.className === 'inner-arrow' && count < 30) {
+      document.getElementById('button').classList.remove('secondspin')
       count++;
       window.location.href = `#event_${count}`;
-    } else if (evt.target.className === 'inner-arrow' && count >= 29) {
-      count = 0;
+      console.log(count);
+    } else if (evt.target.className === 'inner-arrow' && count === 30) {
+      count++;
+      document.getElementById('button').classList.add('spin')
       window.location.href = `#event_${count}`;
+      console.log(count);
+    } else if (evt.target.className === 'inner-arrow' && count > 30) {
+      count = 0;
+      document.getElementById('button').classList.remove('spin');
+      document.getElementById('button').classList.add('secondspin')
+      window.location.href = `#event_${count}`;
+      console.log(count);
     } else {
       console.log('oops');
     }
